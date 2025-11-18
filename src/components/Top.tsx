55d87fa296a8731bf474d301c6f35ad67f628b52
Zach Schwartz <zachatorcodes@gmail.com>
2025-11-18 16:26:56 -0700

    Import and render logo in-line with text

--- src/components/Top.tsx
+++ src/components/Top.tsx
@@ -1,14 +1,18 @@
+import steamLogo from "../assets/steam-logo.png"
+
 function Top() {
   return (
     <>
       <div className="hero bg-base-200 h-50">
         <div className="hero-content text-center">
+          <img src={steamLogo} className="steam-logo"/>
           <div className="max-w-md">
             <h1 className="text-5xl pb-2 font-bold">Steam Sales</h1>
             <p className="pt-2">
               Gaming is fun, but it shouldn't break the bank.
             </p>
           </div>
+          <img src={steamLogo} className="steam-logo"/>
         </div>
       </div>
     </>
