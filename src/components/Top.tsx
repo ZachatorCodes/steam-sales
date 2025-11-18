90cae20fa1eb882d11993e22f4f913f4a89a195c
Zach Schwartz <zachatorcodes@gmail.com>
2025-11-18 16:50:56 -0700

    Move width from css file to inline tailwind

--- src/components/Top.tsx
+++ src/components/Top.tsx
@@ -5,14 +5,14 @@ function Top() {
     <>
       <div className="hero bg-base-200 h-50">
         <div className="hero-content text-center">
-          <img src={steamLogo} className="steam-logo"/>
+          <img src={steamLogo} className="w-25"/>
           <div className="max-w-md">
             <h1 className="text-5xl pb-2 font-bold">Steam Sales</h1>
             <p className="pt-2">
               Gaming is fun, but it shouldn't break the bank.
             </p>
           </div>
-          <img src={steamLogo} className="steam-logo"/>
+          <img src={steamLogo} className="w-25"/>
         </div>
       </div>
     </>
