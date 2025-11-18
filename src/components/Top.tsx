8471f43290db6384ddcef1d689755f2ddf5a4b9e
Zach Schwartz <zachatorcodes@gmail.com>
2025-11-18 16:16:10 -0700

    Create new component for page heading

---
+++ src/components/Top.tsx
@@ -0,0 +1,18 @@
+function Top() {
+  return (
+    <>
+      <div className="hero bg-base-200 h-50">
+        <div className="hero-content text-center">
+          <div className="max-w-md">
+            <h1 className="text-5xl pb-2 font-bold">Steam Sales</h1>
+            <p className="pt-2">
+              Gaming is fun, but it shouldn't break the bank.
+            </p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Top;
